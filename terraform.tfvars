6ad2b99588a62908a803b0bdd8189c5a4c8ae6f0
martin kipchirchir <Martin-254>
2023-07-14 06:00:10 -0400

    terraform

---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+my_region  = "us-east-1"
+profile    = "default"
+topic_name = "sns-dev"
+endpoint   = "uhajij@gmail.com"
+protocol   = "email"
+team       = "martin"
---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "my_region" {
+  type    = string
+  default = "us-east-1"
+
+}
+variable "profile" {
+  default = "default"
+
+}
+variable "topic_name" {
+  default = "sns-dev"
+
+}
+variable "team" {
+  default = "dev"
+
+}
+variable "endpoint" {
+  default = "ksdl@gmail.com"
+
+}
+variable "protocol" {
+  default = "email"
+
+}
